306552880336935f1f1fa0b601ef66ff828d822a
Allison Korkola <allisonkorkola@outlook.com>
2023-08-04 01:24:42 -0700

    Iteration 2 Complete

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -1,8 +1,12 @@
 import SwiftUI
-
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var context
     
-    @State var toDoItems: [ToDoItem] = []
+    @FetchRequest(
+            entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+        
+    var toDoItems: FetchedResults<ToDo>
+    
     @State private var showNewTask = false
     
     var body: some View {
@@ -32,19 +36,32 @@ struct ContentView: View {
      
         List {
                 ForEach (toDoItems) { toDoItem in
-                        if toDoItem.isImportant == true {
-                            Text("‼️" + toDoItem.title)
-                        } else {
-                            Text(toDoItem.title)
-                        }
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + (toDoItem.title ?? "No title"))
+                    } else {
+                                        Text(toDoItem.title ?? "No title")
                     }
+                }
+                .onDelete(perform: deleteTask)
         }
         .listStyle(.plain)
         
         if showNewTask {
-            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+            NewToDoView(title: "", isImportant: false, showNewTask: $showNewTask)
         }
     }
+    
+    private func deleteTask(offsets: IndexSet) {
+            withAnimation {
+                offsets.map { toDoItems[$0] }.forEach(context.delete)
+
+                do {
+                    try context.save()
+                } catch {
+                    print(error)
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
